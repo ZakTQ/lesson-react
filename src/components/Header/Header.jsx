f1cf129c3546418d5bf4e93a982de46bd93b8352
ZakTQ <108540839+ZakTQ@users.noreply.github.com>
2022-10-08 10:12:45 +0300

    14lesson-css-module

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import s from './../Header/Header.module.css'
 
 const Header = () => {
     return (
         <header className='header'>
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/NASA_logo.svg/200px-NASA_logo.svg.png" alt="" />
+            <img className={s.img} src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/NASA_logo.svg/200px-NASA_logo.svg.png" alt="" />
         </header>
     );
 };
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,29 +1,30 @@
 import React from 'react';
+import s from './Nav.module.css'
 
 const Nav = () => {
     return (
         <nav className='nav'>
             <div className='profile'>
-                <a href="s#">
+                <a className={s.link} href="s#">
                     Profile
                 </a>
             </div>
             <div className='messages'>
-                <a href="s#">
+                <a className={s.link} href="s#">
                     Messages
                 </a>
             </div>
             <div className='news'>
-                <a href="s#">
+                <a className={s.link} href="s#">
                     News
                 </a>
             </div>
             <div className='music'>
-                <a href="s#">
+                <a className={`${s.link} + ${s.active}`} href="s#">
                     Music
                 </a>
             </div>
-        </nav>
+        </nav >
     );
 };
 
